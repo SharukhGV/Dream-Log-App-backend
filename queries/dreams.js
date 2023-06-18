7697d7d5c842df3e9d3954035d6b0b25cea9f034
SharukhGV <sharukhvelupillai@pursuit.org>
2023-06-18 01:12:53 -0400

    dream queries fix userId addition

--- queries/dreams.js
+++ queries/dreams.js
@@ -1,14 +1,14 @@
 const db = require("../db/dbConfig.js");
-const admin = require('firebase-admin');
+// const admin = require('firebase-admin');
 // const pgp = require('pg-promise')();
 // const connectionString = process.env.DATABASE_URL;
 // const db = pgp(connectionString);
 
 // Initialize the Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  // Add other necessary configurations
-});
+// admin.initializeApp({
+//   credential: admin.credential.applicationDefault(),
+//   // Add other necessary configurations
+// });
 
 const getAllDreams = async () => await db.any("SELECT * FROM dreams");
 
@@ -25,11 +25,11 @@ const getOneDream = async (id) =>
 // };
 
 const updateOneDream = async (id, dream) => {
-  const { name, good_dream, dream_description, topic, date, night} = dream;
+  const { user_id, name, good_dream, dream_description, topic, date, night} = dream;
 
   return await db.one(
-    "UPDATE dreams SET name=$1, good_dream=$2, dream_description=$3, topic=$4, date=$5, night=$6 WHERE id=$7 RETURNING *",
-    [name, good_dream, dream_description, topic, date, night, id]
+    "UPDATE dreams SET user_id=$1, name=$2, good_dream=$3, dream_description=$4, topic=$5, date=$6, night=$7 WHERE id=$8 RETURNING *",
+    [user_id, name, good_dream, dream_description, topic, date, night, id]
   );
 };
 
@@ -59,14 +59,14 @@ const deleteDream = async (id) =>
 
   const createDream = async (dream) => {
     // Retrieve the user ID token
-    const userToken = await admin.auth().verifyIdToken(dream.user_id);
-    const userId = userToken.uid;
+    // const userToken = await admin.auth().verifyIdToken(dream.user_id);
+    // const userId = userToken.uid;
   
     // Insert data into the database
     const insertedDream = await db.one(
       "INSERT INTO dreams (user_id, name, good_dream, dream_description, topic, date, night) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
       [
-        userId,
+        dream.user_id,
         dream.name,
         dream.good_dream,
         dream.dream_description,
