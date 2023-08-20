673b9e9152e33c269ac686e24cf8e7947cc82081
SharukhGV <sharukhvelupillai@pursuit.org>
2023-08-19 22:42:13 -0400

    cockroach db integration

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,38 +1,68 @@
-// module.exports = {
-//       user: "postgres",
-//       host: "localhost",
-//       database: "nodeapp",
-//       password: "yourPassword",
-//       port: 5432,
+
+
+
+const { Client } = require('pg'); // PostgreSQL library for Node.js
+
+const connectionString = 'postgresql://sharukh:NyQS1FjwpYclFJph3FwOWg@dream-log-app-full-stack-5052.g8z.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full';
+
+const client = new Client({
+  connectionString: connectionString
+});
+
+client.connect()
+  .then(() => {
+    console.log('Connected to CockroachDB');
+  })
+  .catch(error => {
+    console.error('Error connecting to CockroachDB:', error);
+  });
+
+module.exports = client;
+
+
+
+
+
+
+
+
+// // module.exports = {
+// //       user: "postgres",
+// //       host: "localhost",
+// //       database: "nodeapp",
+// //       password: "yourPassword",
+// //       port: 5432,
+// //     };
+
+//     // File required for local and hosted apps
+
+// // http://vitaly-t.github.io/pg-promise/module-pg-promise.html
+
+
+// const pgp = require("pg-promise")();
+// require("dotenv").config();
+
+// // const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
+// const cn = process.env.DATABASE_URL
+//   ? {
+//       connectionString: process.env.DATABASE_URL,
+//       max: 30,
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     }
+//   : {
+//       host: process.env.PG_HOST,
+//       port: process.env.PG_PORT,
+//       database: process.env.PG_DATABASE,
+//       user: process.env.PG_USER,
 //     };
 
-    // File required for local and hosted apps
-
-// http://vitaly-t.github.io/pg-promise/module-pg-promise.html
+// console.log(cn)
 
 
-const pgp = require("pg-promise")();
-require("dotenv").config();
+// const db = pgp(cn);
 
-// const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
-const cn = process.env.DATABASE_URL
-  ? {
-      connectionString: process.env.DATABASE_URL,
-      max: 30,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    }
-  : {
-      host: process.env.PG_HOST,
-      port: process.env.PG_PORT,
-      database: process.env.PG_DATABASE,
-      user: process.env.PG_USER,
-    };
-
-console.log(cn)
+// module.exports = db;
 
 
-const db = pgp(cn);
-
-module.exports = db;
--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 // const passport = require("passport");
 // require("./db/passportConfig")(passport);
 const app = require("./index.js");
+// const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'https://chic-kitsune-7beb31.netlify.app',
+  clientID: 'cNTkBaEqR4hmAsZJ1o1PkcCWTFBtQ8vG',
+  issuerBaseURL: 'https://dev-xz06mncmhh2gwqjf.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
+
+
+
 
 // app.use(function(req, res, next) {
 //   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
 //      next();
+// });
+
+// const checkJwt = auth({
+//   audience: '{yourApiIdentifier}',
+//   issuerBaseURL: `https://{yourDomain}/`,
 // });
 
 require("dotenv").config();
